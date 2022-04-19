bee8b8714e42aa213829aad4130d95fb8aedab40
Micamika <micamika@qq.com>
2022-04-19 09:06:59 +0000

    用户退出登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -37,7 +38,7 @@ class UsersController extends Controller
         ]);
 
         Auth::login($user);
-        
+
         session()->flash('success', '欢迎, 您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
